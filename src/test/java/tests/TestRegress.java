3bba5b77ad3841befb3cc8c02a87c1b832b5170e
VVasluian <vadim.vasluian@orange.com>
2024-05-18 12:41:12 +0300

    First test commit

--- src/test/java/tests/TestRegress.java
+++ src/test/java/tests/TestRegress.java
@@ -2,9 +2,17 @@ package tests;
 
 import org.testng.annotations.Test;
 
+import static io.restassured.RestAssured.given;
+
 public class TestRegress {
     @Test
-    public void firstTest(){
-        System.out.println("First test");
+    public void testListOf(){
+        given()
+                .when()
+                .get("https://reqres.in/api/users?page=2")
+                .then()
+                .assertThat()
+                .statusCode(200);
+
     }
 }
