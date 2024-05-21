d9e5763930fee371ef41387530dd5b9c3eb0016c
VVasluian <vadim.vasluian@orange.com>
2024-05-21 18:05:23 +0300

    Third test commit

--- src/test/java/tests/TestRegress.java
+++ src/test/java/tests/TestRegress.java
@@ -1,13 +1,12 @@
 package tests;
 
-import io.qameta.allure.Allure;
-import io.restassured.RestAssured;
+
 import io.restassured.response.Response;
 import org.testng.annotations.BeforeSuite;
 import org.testng.annotations.Test;
 
 import static io.restassured.RestAssured.*;
-import static utils.Steps.GET;
+import static utils.Steps.*;
 
 public class TestRegress {
     @BeforeSuite
@@ -19,14 +18,27 @@ public class TestRegress {
     public void testListOf(){
         String url = "/users?page=2";
         Response response = GET(url);
-        response.then().assertThat().statusCode(200);
+        isStatusCodeCorrect(response,200);
 
     }
     @Test
     public void getSingleUser(){
         String url = "/users/2";
         Response response = GET(url);
-        response.then().assertThat().statusCode(200);
+        isStatusCodeCorrect(response,200);
+
+    }
+    @Test
+    public void testCreate(){
+        String url ="/users";
+
+        String body="{\n" +
+                "    \"name\": \"morpheus\",\n" +
+                "    \"job\": \"leader\"\n" +
+                "}";
+        Response response = POST(body,url);
+        isStatusCodeCorrect(response,201);
+        isBodyContain(response,"name","name");
 
     }
 }
--- src/test/java/utils/Steps.java
+++ src/test/java/utils/Steps.java
@@ -1,18 +1,41 @@
 package utils;
 
 import io.qameta.allure.Allure;
+import io.qameta.allure.Step;
 import io.restassured.response.Response;
+
 import static io.restassured.RestAssured.*;
 
 import static io.restassured.RestAssured.baseURI;
+import static org.hamcrest.core.IsEqual.equalTo;
 
 public class Steps {
-    public static Response GET(String endpoint){
+    @Step
+    public static Response GET(String endpoint) {
         Allure.addAttachment("URL", baseURI + endpoint);
         Response response = get(endpoint);
         Allure.addAttachment("Response body", response.body().prettyPrint());
         Allure.addAttachment("Status code", String.valueOf(response.statusCode()));
 
         return response;
+    }
+
+    @Step
+    public static void isStatusCodeCorrect(Response response, int expectStatusCode) {
+        response.then().assertThat().statusCode(expectStatusCode);
+    }
+
+    @Step
+    public static Response POST(String body, String endpoint) {
+        Allure.addAttachment("URL", baseURI + endpoint);
+        Allure.addAttachment("Request body", body);
+        Response response = given().body(body).post(endpoint);
+        Allure.addAttachment("Status code", String.valueOf(response.statusCode()));
+        Allure.addAttachment("Response body", response.body().prettyPrint());
+        return response;
+    }
+    @Step
+    public static void isBodyContain(Response response, String field , String expectedField){
+        response.then().assertThat().body(field, equalTo(expectedField));
     }
 }
